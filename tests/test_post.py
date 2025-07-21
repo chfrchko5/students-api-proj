3410157fbccfd74befb327f36049f67685da6a9c
chfrchko5 <r4ger7@gmail.com>
2025-07-21 17:31:03 -0400

    removed debug mode and added one test just to have one

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,8 +1,13 @@
 import json
-from app.model import Student
 
-def test_post(test_client):
+def test_post_get(test_client):
     new_data = {"first_name": "TEST", "last_name": "TEST", "grade": 10}
 
-    response = test_client.post('/api/v1/students', data=json.dumps(new_data), content_type='application/json')
-    assert response.status_code == 201
+    post_response = test_client.post('/api/v1/students', data=json.dumps(new_data), content_type='application/json')
+    assert post_response.status_code == 201
+
+    get_response_data = json.loads(post_response.data)
+    student_id = get_response_data['id']
+
+    get_response = test_client.get(f'/api/v1/students/{student_id}')
+    assert get_response.status_code == 200
