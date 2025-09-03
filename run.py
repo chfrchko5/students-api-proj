23566880dbf8db8a6c1c663ff7f278565bc90df6
chfrchko5 <r4ger7@gmail.com>
2025-09-03 10:22:39 -0400

    metrics port app

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 # set up flask app
 app = create_app()
 metrics = PrometheusMetrics(app)
+metrics.start_http_server(port=5001)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
