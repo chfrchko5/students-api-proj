287778c52ad663d17b7d44e73214626ba8808029
chfrchko5 <r4ger7@gmail.com>
2025-09-03 10:51:08 -0400

    undo

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 from app import create_app
-from prometheus_flask_exporter import PrometheusMetrics
 
 # set up flask app
 app = create_app()
-metrics = PrometheusMetrics(app)
-metrics.start_http_server(port=5001)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
